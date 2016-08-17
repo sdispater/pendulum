46f14b6e125396a16bf21665a56ca910010faf59
YomoFuno <yomofuno@hannex.de>
2016-08-17 16:35:35 +0200

    Update pt_br.py (#19)
    Changed name for 5th day from Quexta-feira to Sexta-feira.

--- pendulum/lang/pt_br.py
+++ pendulum/lang/pt_br.py
@@ -8,7 +8,7 @@ translations = {
         2: 'Terça-feira',
         3: 'Quarta-feira',
         4: 'Quinta-feira',
-        5: 'Qexta-feira',
+        5: 'Sexta-feira',
         6: 'Sábado'
     },
     'days_abbrev': {
