297d1e3190abf5a77726af2d60e67e023e9602e7
Josh Klar <j@iv597.com>
2016-08-22 20:38:32 -0500

    Add pendulumify function and decorator
    This function (and related decorator) will recursively replace all
    datetime instances with Pendulum instances, including those returned by
    generator objects.

---
+++ pendulum/contrib/pendulumify.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    absolute_import,
+    division,
+    unicode_literals,
+    print_function,
+)
+
+from functools import wraps
+
+import inspect
+import pendulum
+
+
+class WrappedGenerator(object):
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __iter__(self):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        return self.next(*args, **kwargs)
+
+    def next(self):
+        return pendulumify(next(self.wrapped))
+
+
+def pendulumify(obj):
+    '''
+
+    Recursively turns all datetime objects in a container type iterable (think
+    dictionaries, lists, sets, etc.) into a Pendulum object, including those
+    returned by generator objects
+
+    '''
+
+    def is_sequence(arg):
+        return (not hasattr(arg, 'strip') and
+                (hasattr(arg, '__getitem__') or
+                hasattr(arg, '__iter__')))
+
+    if not is_sequence(obj):
+        try:
+            return pendulum.instance(obj)
+        except AttributeError:
+            return obj
+
+    if inspect.isgenerator(obj):
+        return WrappedGenerator(obj)
+
+    if isinstance(obj, dict):
+        return {k: pendulumify(v) for k, v in obj.items()}
+
+    constructor = type(obj)
+
+    return constructor(map(pendulumify, obj))
+
+
+def pendulumify_wrap(f):
+    '''
+    Pendulumify, as a decorator.
+    '''
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        return pendulumify(f(*args, **kwargs))
+
+    return wrap
---
+++ tests/contrib_tests/test_pendulumify.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from .. import AbstractTestCase
+from pendulum.contrib.pendulumify import pendulumify, pendulumify_wrap
+
+
+class PendulumifyTest(AbstractTestCase):
+
+    def test_dictionary(self):
+        d = pendulumify({
+            'example': datetime.datetime.now(),
+            'donottouch': 'safe',
+        })
+
+        self.assertIsInstanceOfPendulum(d.get('example'))
+        self.assertEqual(d.get('donottouch'), 'safe')
+
+        @pendulumify_wrap
+        def wrapped():
+            return {
+                'example': datetime.datetime.now(),
+                'donottouch': 'safe',
+            }
+
+        self.assertIsInstanceOfPendulum(wrapped().get('example'))
+        self.assertEqual(wrapped().get('donottouch'), 'safe')
+
+    def test_nested_dictionary(self):
+        d = pendulumify({
+            'inner': {
+                'example': datetime.datetime.now(),
+                'donottouch': 'safe',
+            },
+        })
+
+        self.assertIsInstanceOfPendulum(d.get('inner').get('example'))
+        self.assertEqual(d.get('inner').get('donottouch'), 'safe')
+
+        @pendulumify_wrap
+        def wrapped():
+            return {
+                'inner': {
+                    'example': datetime.datetime.now(),
+                    'donottouch': 'safe',
+                },
+            }
+
+        self.assertIsInstanceOfPendulum(wrapped().get('inner').get('example'))
+        self.assertEqual(wrapped().get('inner').get('donottouch'), 'safe')
+
+    def test_list(self):
+        d = pendulumify([datetime.datetime.now()])
+
+        self.assertIsInstanceOfPendulum(d[0])
+
+        @pendulumify_wrap
+        def wrapped():
+            return [datetime.datetime.now()]
+
+        self.assertIsInstanceOfPendulum(wrapped()[0])
+
+    def test_nested_list(self):
+        d = pendulumify([[datetime.datetime.now()]])
+
+        self.assertIsInstanceOfPendulum(d[0][0])
+
+        @pendulumify_wrap
+        def wrapped():
+            return [[datetime.datetime.now()]]
+
+        self.assertIsInstanceOfPendulum(wrapped()[0][0])
+
+    def test_set(self):
+        d = pendulumify(set([datetime.datetime.now()]))
+
+        self.assertIsInstanceOfPendulum(d.pop())
+
+        @pendulumify_wrap
+        def wrapped():
+            return set([datetime.datetime.now()])
+
+        self.assertIsInstanceOfPendulum(wrapped().pop())
+
+    def test_nested_set(self):
+        d = pendulumify(set([frozenset([datetime.datetime.now()])]))
+
+        self.assertIsInstanceOfPendulum(next(iter(d.pop())))
+
+        @pendulumify_wrap
+        def wrapped():
+            return set([frozenset([datetime.datetime.now()])])
+
+        self.assertIsInstanceOfPendulum(next(iter(wrapped().pop())))
+
+    def test_generator(self):
+        try:
+            xrange
+        except NameError:
+            xrange = range
+
+        d = pendulumify((datetime.datetime.now() for x in xrange(0, 10)))
+
+        for x in d:
+            self.assertIsInstanceOfPendulum(x)
+
+        @pendulumify_wrap
+        def wrapped():
+            def my_gen():
+                for x in xrange(0, 10):
+                    yield datetime.datetime.now()
+
+            return my_gen()
+
+        for x in wrapped():
+            self.assertIsInstanceOfPendulum(x)
+
+    def test_dictionary_from_generator(self):
+        try:
+            xrange
+        except NameError:
+            xrange = range
+
+        d = pendulumify(
+            ({'val': datetime.datetime.now()} for x in xrange(0, 10))
+        )
+
+        for x in d:
+            self.assertIsInstanceOfPendulum(x.get('val'))
+
+        @pendulumify_wrap
+        def wrapped():
+            def my_gen():
+                for x in xrange(0, 10):
+                    yield {'val': datetime.datetime.now()}
+
+            return my_gen()
+
+        for x in wrapped():
+            self.assertIsInstanceOfPendulum(x.get('val'))
