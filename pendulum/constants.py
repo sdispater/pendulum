bf3f4422f779380492dc3e5e0e4be1cb599eb75f
Sébastien Eustace <sebastien@eustace.io>
2017-01-11 13:21:40 -0500

    Makes list constants immutable (tuples)

--- pendulum/constants.py
+++ pendulum/constants.py
@@ -53,37 +53,37 @@ SECS_PER_400_YEARS = 146097 * SECS_PER_DAY
 
 # The number of seconds in an aligned 100-year chunk, for those that
 # do not begin with a leap year and those that do respectively.
-SECS_PER_100_YEARS = [
+SECS_PER_100_YEARS = (
     (76 * DAYS_PER_N_YEAR + 24 * DAYS_PER_L_YEAR) * SECS_PER_DAY,
-    (75 * DAYS_PER_N_YEAR + 25 * DAYS_PER_L_YEAR) * SECS_PER_DAY,
-]
+    (75 * DAYS_PER_N_YEAR + 25 * DAYS_PER_L_YEAR) * SECS_PER_DAY
+)
 
 # The number of seconds in an aligned 4-year chunk, for those that
 # do not begin with a leap year and those that do respectively.
-SECS_PER_4_YEARS = [
+SECS_PER_4_YEARS = (
     (4 * DAYS_PER_N_YEAR + 0 * DAYS_PER_L_YEAR) * SECS_PER_DAY,
-    (3 * DAYS_PER_N_YEAR + 1 * DAYS_PER_L_YEAR) * SECS_PER_DAY,
-]
+    (3 * DAYS_PER_N_YEAR + 1 * DAYS_PER_L_YEAR) * SECS_PER_DAY
+)
 
 # The number of seconds in non-leap and leap years respectively.
-SECS_PER_YEAR = [
+SECS_PER_YEAR = (
     DAYS_PER_N_YEAR * SECS_PER_DAY,
-    DAYS_PER_L_YEAR * SECS_PER_DAY,
-]
+    DAYS_PER_L_YEAR * SECS_PER_DAY
+)
 
 # The month lengths in non-leap and leap years respectively.
-DAYS_PER_MONTHS = [
-    [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-    [-1, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-]
+DAYS_PER_MONTHS = (
+    (-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31),
+    (-1, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+)
 
 # The day offsets of the beginning of each (1-based) month in non-leap
 # and leap years respectively.
 # For example, in a leap year there are 335 days before December.
-MONTHS_OFFSETS = [
-    [-1, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365],
-    [-1, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
-]
+MONTHS_OFFSETS = (
+    (-1, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365),
+    (-1, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366)
+)
 
 TM_SUNDAY = 0
 TM_MONDAY = 1
