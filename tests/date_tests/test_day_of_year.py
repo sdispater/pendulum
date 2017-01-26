7923a6a7ed25c21d9f85cad988785d1e4eeb9559
Laurent Rahuel <laurent.rahuel@gmail.com>
2017-01-26 20:21:29 +0100

    Adding a simple unit test for day_of_year (leap or not)

---
+++ tests/date_tests/test_day_of_year.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import pendulum
+from pendulum.exceptions import PendulumException
+
+from .. import AbstractTestCase
+
+
+class DayOfYearTest(AbstractTestCase):
+    def test_non_leap_year(self):
+        d = pendulum.create(1975, 3, 22)
+        self.assertEqual(
+            d.day_of_year, 81
+        )
+
+    def test_leap_year(self):
+        d = pendulum.create(1972, 4, 24)
+        self.assertEqual(
+            d.day_of_year, 115
+        )
