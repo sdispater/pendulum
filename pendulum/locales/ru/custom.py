e2a0ea573c45df12b28af777271e3d6e3a2dbc45
Senpos <kovalchuk.evgeny@gmail.com>
2018-06-06 22:49:50 +0300

    Add RU locale

---
+++ pendulum/locales/ru/custom.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""
+ru custom locale file.
+"""
+
+translations = {
+    'units': {
+        'few_second': 'несколько секунд'
+    },
+
+    # Relative time
+    'ago': '{} назад',
+    'from_now': 'через {}',
+    'after': '{0} после',
+    'before': '{0} до',
+
+    # Date formats
+    'date_formats': {
+        'LTS': 'HH:mm:ss',
+        'LT': 'HH:mm',
+        'L': 'DD.MM.YYYY',
+        'LL': 'D MMMM YYYY г.',
+        'LLL': 'D MMMM YYYY г., HH:mm',
+        'LLLL': 'dddd, D MMMM YYYY г., HH:mm'
+    },
+}
