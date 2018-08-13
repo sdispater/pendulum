e62bd1999500d9636d9ce7f48334b0457624cc06
widnyana <wid@widnyana.web.id>
2018-08-13 18:58:31 +0700

    add id_ID localization

---
+++ pendulum/locales/id/custom.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""
+id custom locale file.
+"""
+
+translations = {
+    'units': {
+        'few_second': 'beberapa menit'
+    },
+
+    # Relative time
+    'ago': '{} lalu',
+    'from_now': 'dalam {}',
+    'after': '{0} kemudian',
+    'before': '{0} yang lalu',
+
+    # Ordinals
+    'ordinal': {},
+
+    # Date formats
+    'date_formats': {
+        'LTS': 'h:mm:ss A',
+        'LT': 'h:mm A',
+        'L': 'MM/DD/YYYY',
+        'LL': 'MMMM D, YYYY',
+        'LLL': 'MMMM D, YYYY h:mm A',
+        'LLLL': 'dddd, MMMM D, YYYY h:mm A',
+    },
+}
+
