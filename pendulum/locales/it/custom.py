2b40b262c9b75d9ca9eb7f0387f5828279b18bbf
Daniel Kertesz <daniel@spatof.org>
2019-02-09 00:08:34 +0000

    Add "it" localization

---
+++ pendulum/locales/it/custom.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+"""
+it custom locale file.
+"""
+
+translations = {
+    "units": {"few_second": "qualche secondo"},
+    # Relative time
+    "ago": "{} fa",
+    "from_now": "tra {}",
+    "after": "{0} dopo",
+    "before": "{0} prima",
+    # Ordinals
+    "ordinal": {"other": "º"},
+    # Date formats
+    "date_formats": {
+        "LTS": "h:mm:ss A",
+        "LT": "h:mm A",
+        "L": "DD/MM/YYYY",
+        "LL": "D MMMM YYYY",
+        "LLL": "D MMMM YYYY h:mm A",
+        "LLLL": "dddd D MMMM YYYY h:mm A",
+    },
+}
---
+++ tests/localization/test_it.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import pendulum
+
+
+locale = "it"
+
+
+def test_diff_for_humans():
+    with pendulum.test(pendulum.datetime(2016, 8, 29)):
+        diff_for_humans()
+
+
+def diff_for_humans():
+    d = pendulum.now().subtract(seconds=1)
+    assert d.diff_for_humans(locale=locale) == "qualche secondo fa"
+
+    d = pendulum.now().subtract(seconds=2)
+    assert d.diff_for_humans(locale=locale) == "qualche secondo fa"
+
+    d = pendulum.now().subtract(minutes=1)
+    assert d.diff_for_humans(locale=locale) == "1 minuto fa"
+
+    d = pendulum.now().subtract(minutes=2)
+    assert d.diff_for_humans(locale=locale) == "2 minuti fa"
+
+    d = pendulum.now().subtract(hours=1)
+    assert d.diff_for_humans(locale=locale) == "1 ora fa"
+
+    d = pendulum.now().subtract(hours=2)
+    assert d.diff_for_humans(locale=locale) == "2 ore fa"
+
+    d = pendulum.now().subtract(days=1)
+    assert d.diff_for_humans(locale=locale) == "1 giorno fa"
+
+    d = pendulum.now().subtract(days=2)
+    assert d.diff_for_humans(locale=locale) == "2 giorni fa"
+
+    d = pendulum.now().subtract(weeks=1)
+    assert d.diff_for_humans(locale=locale) == "1 settimana fa"
+
+    d = pendulum.now().subtract(weeks=2)
+    assert d.diff_for_humans(locale=locale) == "2 settimane fa"
+
+    d = pendulum.now().subtract(months=1)
+    assert d.diff_for_humans(locale=locale) == "1 mese fa"
+
+    d = pendulum.now().subtract(months=2)
+    assert d.diff_for_humans(locale=locale) == "2 mesi fa"
+
+    d = pendulum.now().subtract(years=1)
+    assert d.diff_for_humans(locale=locale) == "1 anno fa"
+
+    d = pendulum.now().subtract(years=2)
+    assert d.diff_for_humans(locale=locale) == "2 anni fa"
+
+    d = pendulum.now().add(seconds=1)
+    assert d.diff_for_humans(locale=locale) == "tra qualche secondo"
+
+    d = pendulum.now().add(seconds=1)
+    d2 = pendulum.now()
+    assert d.diff_for_humans(d2, locale=locale) == "qualche secondo dopo"
+    assert d2.diff_for_humans(d, locale=locale) == "qualche secondo prima"
+
+    assert d.diff_for_humans(d2, True, locale=locale) == "qualche secondo"
+    assert d2.diff_for_humans(d.add(seconds=1), True, locale=locale) == "qualche secondo"
