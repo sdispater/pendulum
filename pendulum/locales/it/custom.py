c17eb8cd3894217b1ab8f16fc8b5192676796e69
Michele Comitini <mcm@glisco.it>
2019-08-08 14:06:24 +0200

    Add italian [it] locale.

---
+++ pendulum/locales/it/custom.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+it custom locale file.
+"""
+
+from __future__ import unicode_literals
+
+
+translations = {
+    # Date formats
+    "date_formats": {
+        "LTS": "H:mm:ss",
+        "LT": "H:mm",
+        "L": "DD/MM/YYYY",
+        "LL": "D MMMM YYYY",
+        "LLL": "D MMMM YYYY [alle] H:mm",
+        "LLLL": "dddd, D MMMM YYYY [alle] H:mm",
+    },
+}
