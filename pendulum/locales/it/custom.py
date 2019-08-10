4bf1bf1e79388bfdde11436f74ab58a78c0036e6
Michele Comitini <mcm@glisco.it>
2019-08-10 22:25:41 +0200

    Added tests.

--- pendulum/locales/it/custom.py
+++ pendulum/locales/it/custom.py
@@ -7,6 +7,14 @@ from __future__ import unicode_literals
 
 
 translations = {
+    "units": {"few_second": "alcuni secondi"},
+    # Relative Time
+    "ago": "{0} fa",
+    "from_now": "in {0}",
+    "after": "{0} dopo",
+    "before": "{0} prima",
+    # Ordinals
+    "ordinal": {"other": "°"},
     # Date formats
     "date_formats": {
         "LTS": "H:mm:ss",
