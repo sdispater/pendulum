dc488c7d7c90a9b4cce231e3939dbfbb4a2d5b7b
Sébastien Eustace <sebastien_eustace@ultimatesoftware.com>
2020-07-17 18:40:46 +0200

    Improve C extensions build

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import os
+import shutil
 import sys
 
 from distutils.command.build_ext import build_ext
+from distutils.core import Distribution
 from distutils.core import Extension
 from distutils.errors import CCompilerError
 from distutils.errors import DistutilsExecError
@@ -33,27 +35,53 @@ class BuildFailed(Exception):
 class ExtBuilder(build_ext):
     # This class allows C extension building to fail.
 
+    built_extensions = []
+
     def run(self):
         try:
             build_ext.run(self)
         except (DistutilsPlatformError, FileNotFoundError):
-            print("************************************************************")
-            print("Cannot compile C accelerator module, use pure python version")
-            print("************************************************************")
+            print(
+                "  Unable to build the C extensions, "
+                "Pendulum will use the pure python code instead."
+            )
 
     def build_extension(self, ext):
         try:
             build_ext.build_extension(self, ext)
         except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
-            print("************************************************************")
-            print("Cannot compile C accelerator module, use pure python version")
-            print("************************************************************")
+            print(
+                '  Unable to build the "{}" C extension, '
+                "Pendulum will use the pure python version of the extension.".format(
+                    ext.name
+                )
+            )
 
 
 def build(setup_kwargs):
     """
     This function is mandatory in order to build the extensions.
     """
-    setup_kwargs.update(
-        {"ext_modules": extensions, "cmdclass": {"build_ext": ExtBuilder}}
-    )
+    distribution = Distribution({"name": "pendulum", "ext_modules": extensions})
+    distribution.package_dir = "pendulum"
+
+    cmd = ExtBuilder(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        if not os.path.exists(output):
+            continue
+
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+    return setup_kwargs
+
+
+if __name__ == "__main__":
+    build({})
