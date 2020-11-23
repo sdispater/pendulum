364388d853acf61131de30dcf60ea794b8edb9db
Sean Stewart <sean_stewart@me.com>
2020-11-23 18:47:08 -0500

    Run pre-commit linters on source.

--- tests/duration/test_isoformat.py
+++ tests/duration/test_isoformat.py
@@ -1,24 +1,24 @@
 import pytest
 
-from pendulum import Duration, parse
+from pendulum import Duration
+from pendulum import parse
 
 
 @pytest.mark.parametrize(
     "dur, expected_iso",
     [
         (
-                Duration(
-                    years=1,
-                    months=3,
-                    days=6,
-                    minutes=50,
-                    seconds=3,
-                    milliseconds=10,
-                    microseconds=10,
-                ),
-                "P1Y3M6DT0H50M3.010010S",
+            Duration(
+                years=1,
+                months=3,
+                days=6,
+                minutes=50,
+                seconds=3,
+                milliseconds=10,
+                microseconds=10,
+            ),
+            "P1Y3M6DT0H50M3.010010S",
         ),
-
         (Duration(days=4, hours=12, minutes=30, seconds=5), "P0Y0M4DT12H30M5S"),
         (Duration(days=4, hours=12, minutes=30, seconds=5), "P0Y0M4DT12H30M5S"),
         (Duration(microseconds=10), "P0Y0M0DT0H0M0.000010S"),
