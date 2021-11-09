2e85e3e776e0ce308892e23bcc7ad0df05199782
Zdenko Podobný <zdenop@gmail.com>
2021-11-09 13:00:32 +0100

    add Slovak locale

---
+++ pendulum/locales/sk/custom.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+"""
+sk custom locale file.
+"""
+
+translations = {
+    "units": {"few_second": "pár sekúnd"},
+    # Relative time
+    "ago": "pred {}",
+    "from_now": "o {}",
+    "after": "po {0}",
+    "before": "pred {0}",
+
+    # Date formats
+    "date_formats": {
+        "LTS": "HH:mm:ss",
+        "LT": "HH:mm",
+        "LLLL": "dddd, D. MMMM YYYY HH:mm",
+        "LLL": "D. MMMM YYYY HH:mm",
+        "LL": "D. MMMM YYYY",
+        "L": "DD.MM.YYYY",
+    },
+}
