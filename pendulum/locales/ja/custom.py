ca589aaa85f9e9113e49d5ef37305715f7d44295
za <zakiakhmad@gmail.com>
2022-08-16 14:33:12 +0700

    Added JA translation (#610)
    * Initiate JA translation
    
    * Initiate JA test code
    
    * Update custom.py
    
    * Update assert in the test code
    
    * Reformat locale.py using black
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Bartosz Sokorski <b.sokorski@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ pendulum/locales/ja/custom.py
@@ -0,0 +1,21 @@
+"""
+ja custom locale file.
+"""
+
+translations = {
+    "units": {"few_second": "数秒"},
+    # Relative time
+    "ago": "{} 前に",
+    "from_now": "今から {}",
+    "after": "{0} 後",
+    "before": "{0} 前",
+    # Date formats
+    "date_formats": {
+        "LTS": "h:mm:ss A",
+        "LT": "h:mm A",
+        "L": "MM/DD/YYYY",
+        "LL": "MMMM D, YYYY",
+        "LLL": "MMMM D, YYYY h:mm A",
+        "LLLL": "dddd, MMMM D, YYYY h:mm A",
+    },
+}
