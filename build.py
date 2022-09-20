42865d635f9bdf9cda61de9fc4d25e0568274867
Sébastien Eustace <sebastien@eustace.io>
2022-09-20 22:57:43 +0200

    Use meson to build C extensions

--- build.py
+++ build.py
@@ -1,84 +1,32 @@
-import os
-import shutil
-import sys
+import subprocess
 
-from distutils.command.build_ext import build_ext
-from distutils.core import Distribution
-from distutils.core import Extension
-from distutils.errors import CCompilerError
-from distutils.errors import DistutilsExecError
-from distutils.errors import DistutilsPlatformError
+from pathlib import Path
 
 
-# C Extensions
-with_extensions = os.getenv("PENDULUM_EXTENSIONS", None)
-
-if with_extensions == "1" or with_extensions is None:
-    with_extensions = True
-
-if with_extensions == "0" or hasattr(sys, "pypy_version_info"):
-    with_extensions = False
-
-extensions = []
-if with_extensions:
-    extensions = [
-        Extension("pendulum._extensions._helpers", ["pendulum/_extensions/_helpers.c"]),
-        Extension("pendulum.parsing._iso8601", ["pendulum/parsing/_iso8601.c"]),
-    ]
+def meson(*args):
+    subprocess.call(["meson"] + list(args))
 
 
-class BuildFailed(Exception):
+def _build():
+    build_dir = Path(__file__).parent.joinpath("build")
+    build_dir.mkdir(parents=True, exist_ok=True)
 
-    pass
-
-
-class ExtBuilder(build_ext):
-    # This class allows C extension building to fail.
-
-    built_extensions = []
-
-    def run(self):
-        try:
-            build_ext.run(self)
-        except (DistutilsPlatformError, FileNotFoundError):
-            print(
-                "  Unable to build the C extensions, "
-                "Pendulum will use the pure python code instead."
-            )
-
-    def build_extension(self, ext):
-        try:
-            build_ext.build_extension(self, ext)
-        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
-            print(
-                f'  Unable to build the "{ext.name}" C extension, '
-                "Pendulum will use the pure python version of the extension."
-            )
+    meson("setup", build_dir.as_posix())
+    meson("compile", "-C", build_dir.as_posix())
+    meson("install", "-C", build_dir.as_posix())
 
 
 def build(setup_kwargs):
     """
     This function is mandatory in order to build the extensions.
     """
-    distribution = Distribution({"name": "pendulum", "ext_modules": extensions})
-    distribution.package_dir = "pendulum"
-
-    cmd = ExtBuilder(distribution)
-    cmd.ensure_finalized()
-    cmd.run()
-
-    # Copy built extensions back to the project
-    for output in cmd.get_outputs():
-        relative_extension = os.path.relpath(output, cmd.build_lib)
-        if not os.path.exists(output):
-            continue
-
-        shutil.copyfile(output, relative_extension)
-        mode = os.stat(relative_extension).st_mode
-        mode |= (mode & 0o444) >> 2
-        os.chmod(relative_extension, mode)
-
-    return setup_kwargs
+    try:
+        _build()
+    except Exception:
+        print(
+            "  Unable to build C extensions, "
+            "Pendulum will use the pure python version of the extensions."
+        )
 
 
 if __name__ == "__main__":
