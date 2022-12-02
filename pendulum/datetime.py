e7f34bc2eb984284e5d73426338afdd3bb03af6f
esauser <eric.sauser@gmail.com>
2022-12-01 22:01:31 -0600

    Update datetime.py

--- pendulum/datetime.py
+++ pendulum/datetime.py
@@ -1356,21 +1356,21 @@ class DateTime(datetime.datetime, Date):
     ]:
         return self.__class__, self._getstate(protocol)
 
-    def __le__(self, other: datetime.datetime) -> bool:
+    def __le__(self, other: datetime.date) -> bool:
         if isinstance(other, DateTime):
             return self._cmp(other) <= 0
         return super().__le__(other)
 
-    def __lt__(self, other: datetime.datetime) -> bool:
+    def __lt__(self, other: datetime.date) -> bool:
         if isinstance(other, DateTime):
             return self._cmp(other) < 0
         return super().__lt__(other)
 
-    def __ge__(self, other: datetime.datetime) -> bool:
+    def __ge__(self, other: datetime.date) -> bool:
         # Will default to the negative of its reflection
         return NotImplemented
 
-    def __gt__(self, other: datetime.datetime) -> bool:
+    def __gt__(self, other: datetime.date) -> bool:
         # Will default to the negative of its reflection
         return NotImplemented
 
