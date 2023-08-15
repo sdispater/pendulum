9590c651c5c5ff3bec064abc7aaf270c0c6f5cee
Sébastien Eustace <sebastien@eustace.io>
2023-08-15 15:43:56 +0200

    Fix deepcopy of durations
    Fixes #714

--- tests/duration/test_behavior.py
+++ tests/duration/test_behavior.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 import pickle
 
+from copy import deepcopy
 from datetime import timedelta
 
 import pendulum
 
+from tests.conftest import assert_duration
 
-def test_pickle():
+
+def test_pickle() -> None:
     it = pendulum.duration(days=3, seconds=2456, microseconds=123456)
     s = pickle.dumps(it)
     it2 = pickle.loads(s)
@@ -15,7 +18,15 @@ def test_pickle():
     assert it == it2
 
 
-def test_comparison_to_timedelta():
+def test_comparison_to_timedelta() -> None:
     duration = pendulum.duration(days=3)
 
     assert duration < timedelta(days=4)
+
+
+def test_deepcopy() -> None:
+    duration = pendulum.duration(months=1)
+    copied_duration = deepcopy(duration)
+
+    assert copied_duration == duration
+    assert_duration(copied_duration, months=1)
